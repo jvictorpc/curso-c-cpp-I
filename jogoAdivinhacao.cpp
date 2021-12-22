df99cded9853edb8226e159911e39b54c932e524
victor <jvictorpc@gmail.com>
2021-12-22 11:05:58 -0300

    limitando numero de tentativas

--- jogoAdivinhacao.cpp
+++ jogoAdivinhacao.cpp
@@ -16,11 +16,32 @@ int main (){
     srand(segundos);
     int num_grande = rand();
     numeroSecreto = num_grande % 100;
+    int dificuldade = 0;
+    int num_tentativas = 0;
+
+    printf("Informe o nivel de dificuldade:\n");
+    printf("(1) Facil (2) Medio (3) Dificil\n");
+    scanf("%d", &dificuldade);
+
+    switch(dificuldade){
+        case 1:
+            num_tentativas = 20;
+            break;
+        case 2:
+            num_tentativas = 15;
+            break;
+        case 3:
+            num_tentativas = 6;
+            break;
+        default:
+            printf("Nao eh uma diculdade valida!!!\n\n");
+            break;
+    }
     
     cont = 1;
     pontos = 1000;
 
-    while(1){
+    for(int i = 1; i<= num_tentativas; i++){
 
         printf("Tentativa numero %d \n\n",cont );
 
@@ -37,11 +58,6 @@ int main (){
         maior = chute > numeroSecreto;
 
         if(acertou){
-            printf("vc acertou!!!\n\n");
-            printf("===================================================\n");
-            printf("FIM DE JOGO\n");
-            printf("Voce demorou %d tentativas pra acertar o numero.\n", cont);
-            printf("Voce fez %.2f pontos.\n", pontos);
             break;
         }else if (maior){
             printf("O numero secreto eh menor!!\n\n");
@@ -53,7 +69,18 @@ int main (){
         pontos = pontos - pontos_perdidos;
         cont++;
     }
-    
+
+    if(acertou){
+        printf("vc acertou!!!\n\n");
+        printf("===================================================\n");
+        printf("FIM DE JOGO\n");
+        printf("Voce demorou %d tentativas pra acertar o numero.\n", cont);
+        printf("Voce fez %.2f pontos.\n", pontos);
+    }else{
+        printf("you lose playboy!!! tente denovo.");
+    }
+
+   
 
     system("pause");
     system("cls");    
